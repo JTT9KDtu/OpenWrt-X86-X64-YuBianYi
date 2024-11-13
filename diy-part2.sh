1a724557411bb30751852e1c463322eacfa22e33
JTT9KDtu <zzxusapp@gmail.com>
2024-11-13 13:22:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,3 +7,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/hostname='OpenWrt'/hostname='immortalwrt-XIAOMI-AX3000T'/g' package/base-files/files/bin/config_generate
+
