ec41f52c7b6b8d83c8447b0a6f6b53520ef7fa05
JTT9KDtu <zzxusapp@gmail.com>
2024-11-26 12:20:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,3 +11,10 @@ sed -i 's/192.168.1.1/192.168.89.1/g' package/base-files/files/bin/config_genera
 # sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/OpenWrt24-X86-X64-IPV4/g' package/base-files/files/bin/config_generate
 
+# Step 3: 修改默认时区
+# 将默认时区从 "UTC" 修改为 "CST-8"
+sed -i "s/'UTC'/'CST-8'/g" package/base-files/files/bin/config_generate
+
+# Step 4: 增加时区名称设置为 "Asia/Shanghai"
+# 在时区设置后添加区域名称设置
+sed -i "/set system.\@system\[-1\].timezone='CST-8'/a\ \ \ \ set system.\@system\[-1\].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
